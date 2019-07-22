0bbb7f17fe9a6fe322375d3929906347fcfab7f3
Tedu <577844214@qq.com>
2019-07-22 10:36:43 +0800

    init add

---
+++ pool.py
@@ -0,0 +1,21 @@
+"""
+进程池使用
+"""
+from multiprocessing import Pool
+from time import sleep,ctime
+# 进程池事件
+def worker(msg):
+    sleep(2)
+    print(ctime(),"--",msg)
+
+# 创建进程池
+pool = Pool(10)
+# 添加事件
+for i in range(10):
+    msg = "Tedu %d"%i
+    pool.apply_async(func=worker,args=(msg,))
+
+# 关闭进程池
+pool.close()
+# 回收进程池
+pool.join()
