b0dbcd96ca209cb2aa412f5ed825beb364b8b687
sungod-one <53117549+sungod-one@users.noreply.github.com>
2022-09-20 18:52:02 +0800

    Update github.py

--- github.py
+++ github.py
@@ -1,6 +1,24 @@
-# 这是一个测试代码
+import itertools
 
-print("hello world")
+a_list = [1, 3, 5]
+b_list = [2, 4, 6]
+# 第一步取出a_list、b_list中可能2位数组合
+def get_two_number(_list):
+    res_list = []
+    for index_i, item in enumerate(_list):
+        for index_j in range(index_i + 1, len(_list)):
+            res_list.append((_list[index_i], _list[index_j]))
+    return res_list
+a_two_list = get_two_number(a_list)
+b_two_list = get_two_number(b_list)
+# 全排列组合数字
+res_list = []
+for item_i in a_two_list:
+    for item_j in b_two_list:
+        iter_list = list(item_i) + list(item_j)
+        tmp_list = list(itertools.permutations(iter_list))
+        tmp_list = ["".join(map(str, list(tuple_item))) for tuple_item in tmp_list]
+        res_list += tmp_list
 
-print("修改内容")
-print("fetch 拉取代码")
+print(res_list)
+print(len(res_list))
